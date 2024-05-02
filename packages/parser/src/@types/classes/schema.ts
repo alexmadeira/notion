f5bec778980253ee66d36bf4be927a92bc415fb3
Alex Madeira <alex.c.madeira@gmai.com>
2024-05-02 16:07:12 -0300

    feat(schema): start create schema class

--- packages/parser/src/@types/classes/schema.ts
+++ packages/parser/src/@types/classes/schema.ts
@@ -1,5 +1,21 @@
 import { z } from 'zod'
 
+export const ZSchemaProperty = z.object({
+  key: z.string(),
+  columnName: z.string(),
+})
+
+export const ZSchemaData = z.array(ZSchemaProperty)
+
+export const ZSchemaProps = z.union([ZSchemaProperty, z.array(ZSchemaProperty)])
 export const ZSchema = z.object({})
 
+//
+//
+//
+
+export type TSchemaProperty = z.infer<typeof ZSchemaProperty>
+export type TSchemaData = z.infer<typeof ZSchemaData>
+
+export type TSchemaProps = z.infer<typeof ZSchemaProps>
 export interface ISchema extends z.infer<typeof ZSchema> {}
--- packages/parser/src/classes/Schema.ts
+++ packages/parser/src/classes/Schema.ts
@@ -1,3 +1,24 @@
-import { ISchema } from '@/classes/schema'
+import type {
+  ISchema,
+  TSchemaData,
+  TSchemaProperty,
+  TSchemaProps,
+} from '@/classes/schema'
 
-export class Schema implements ISchema {}
+import { ZSchemaProps } from '@/classes/schema'
+
+import _ from 'lodash'
+
+export class Schema implements ISchema {
+  private _schemaData: TSchemaData = []
+
+  constructor(props: TSchemaProps) {
+    const schema = _.concat([], ZSchemaProps.parse(props))
+
+    this.schemaData(schema)
+  }
+
+  private schemaData(schemaProperties: TSchemaProperty[]) {
+    this._schemaData = _.concat(this._schemaData, schemaProperties)
+  }
+}
